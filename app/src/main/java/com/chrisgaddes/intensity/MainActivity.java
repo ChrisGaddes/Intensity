c27cb57e7283e0d20fff8d88dc1bce13281f402b
ChrisGaddes <cagaddes@gmail.com>
2016-10-01 12:51:43 -0500

    Changed to single EditText from five separate TextViews to display time log

--- app/src/main/java/com/chrisgaddes/intensity/MainActivity.java
+++ app/src/main/java/com/chrisgaddes/intensity/MainActivity.java
@@ -2,24 +2,20 @@ package com.chrisgaddes.intensity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_start_stop;
+    private Button btn_reset;
     private ChronometerView rc;
     private boolean stopped;
     private TinyDB tinydb;
     private int workout_num;
 
-    private TextView tv_workout_1;
-    private TextView tv_workout_2;
-    private TextView tv_workout_3;
-    private TextView tv_workout_4;
-    private TextView tv_workout_5;
+    private EditText tv_workout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +27,10 @@ public class MainActivity extends AppCompatActivity {
         stopped = true;
         workout_num = 1;
 
-        tv_workout_1 = (TextView) findViewById(R.id.tv_workout_1);
-        tv_workout_2 = (TextView) findViewById(R.id.tv_workout_2);
-        tv_workout_3 = (TextView) findViewById(R.id.tv_workout_3);
-        tv_workout_4 = (TextView) findViewById(R.id.tv_workout_4);
-        tv_workout_5 = (TextView) findViewById(R.id.tv_workout_5);
+        tv_workout = (EditText) findViewById(R.id.tv_workout);
 
         btn_start_stop = (Button) findViewById(R.id.btn_start_stop);
+        btn_reset = (Button) findViewById(R.id.btn_reset);
 
         btn_start_stop.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +45,13 @@ public class MainActivity extends AppCompatActivity {
                     btn_start_stop.setText("Start");
                     stopped = !stopped;
                 }
+            }
+        });
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetLog();
             }
         });
     }
@@ -72,7 +72,15 @@ public class MainActivity extends AppCompatActivity {
             rc.stop();
         }
         workout_num = workout_num+1;
+    }
 
+    private void resetLog() {
+        if (rc != null) {
+            rc.reset();
+        }
+        workout_num = 1;
+
+        tv_workout.setText("");
     }
 
     private void addTimeToLog(int mworkout_num,long time){
@@ -81,24 +89,11 @@ public class MainActivity extends AppCompatActivity {
 
         tinydb.putLong(str_name, time);
 
-        String str_textview_time = "Workout " + mworkout_num + ": Time = " + String.valueOf(time) + " seconds";
+        String str_current_text = tv_workout.getText().toString();
 
-        switch(mworkout_num) {
-            case 1:
-                tv_workout_1.setText(str_textview_time);
-                break;
-            case 2:
-                tv_workout_2.setText(str_textview_time);
-                break;
-            case 3:
-                tv_workout_3.setText(str_textview_time);
-                break;
-            case 4:
-                tv_workout_4.setText(str_textview_time);
-                break;
-            case 5:
-                tv_workout_5.setText(str_textview_time);
-                break;
-        }
+        String str_textview_time = str_current_text + "\n\n" + "Workout " + mworkout_num + ": Time = " + String.valueOf(time) + " seconds";
+
+        tv_workout.setText(str_textview_time);
+
     }
 }
